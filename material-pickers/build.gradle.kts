012aab1eb62876592a47dc67ada6097c94989fc1
adam.rabasek@gmail.com <eidam@10.0.0.53>
2025-10-09 00:13:30 +0200

    feat: add settings for publishing to maven central

--- material-pickers/build.gradle.kts
+++ material-pickers/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -43,9 +44,42 @@ afterEvaluate {
         publications {
             create<MavenPublication>("release") {
                 from(components["release"])
+
                 groupId = "com.github.eidam-slices"
                 artifactId = "material-pickers"
+                // new:
+                version = findProperty("VERSION_NAME") as String
+
+                pom {
+                    name.set("Material Pickers")
+                    description.set("Material-styled item pickers for Android - Jetpack Compose")
+                    url.set("https://github.com/eidam-slices/material-pickers")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                            distribution.set("repo")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("eidam-slices")
+                            name.set("Eidam")
+                            url.set("https://github.com/eidam-slices")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/eidam-slices/material-pickers")
+                        connection.set("scm:git@github.com:eidam-slices/material-pickers.git")
+                        developerConnection.set("scm:git@github.com:eidam-slices/material-pickers.git")
+                    }
+
+                }
             }
+        }
+        signing {
+            useGpgCmd()
+            sign(publishing.publications["release"])
         }
     }
 }
