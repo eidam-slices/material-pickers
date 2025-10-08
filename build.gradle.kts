54c6260e937d8ba4afe305932877e829b2da5cad
adam.rabasek@gmail.com <eidam@10.0.0.53>
2025-10-09 00:13:30 +0200

    feat: add settings for publishing to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,16 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            username.set(findProperty("OSSRH_USERNAME") as String)
+            password.set(findProperty("OSSRH_PASSWORD") as String)
+            stagingProfileId.set("io.github.eidam-slices")
+        }
+    }
 }
