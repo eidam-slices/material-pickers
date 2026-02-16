7a5d6f59dfdb3716b411b4ca9047e1f5d6e6e2fa
eidam-slices <adam.rabasek@gmail.com>
2026-02-16 01:18:12 +0100

    configure publishing to maven central with vanniktech-maven-publish

--- material-pickers/build.gradle.kts
+++ material-pickers/build.gradle.kts
@@ -1,94 +1,51 @@
 plugins {
-//    alias(libs.plugins.android.library)
-//    alias(libs.plugins.kotlin.android)
-//    alias(libs.plugins.kotlin.compose)
-    // new
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
 
-    id("maven-publish")
-    id("signing")
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
-//android {
-//    namespace = "cum.eidam.material_pickers"
-//    compileSdk = 36
-//
-//    defaultConfig {
-//        minSdk = 21
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        consumerProguardFiles("consumer-rules.pro")
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_11
-//        targetCompatibility = JavaVersion.VERSION_11
-//    }
-//    kotlinOptions {
-//        jvmTarget = "11"
-//    }
-//
-//    publishing {
-//        singleVariant("release") {
-//            withSourcesJar()
-//            withJavadocJar()
-//        }
-//    }
-//}
+group = "io.github.eidam-slices"
+version = "0.4.0"
 
-//afterEvaluate {
-//    publishing {
-//        publications {
-//            create<MavenPublication>("release") {
-//                from(components["release"])
-//
-//                groupId = "com.github.eidam-slices"
-//                artifactId = "material-pickers"
-//                // new:
-//                version = findProperty("VERSION_NAME") as String
-//
-//                pom {
-//                    name.set("Material Pickers")
-//                    description.set("Material-styled item pickers for Android - Jetpack Compose")
-//                    url.set("https://github.com/eidam-slices/material-pickers")
-//                    licenses {
-//                        license {
-//                            name.set("The Apache License, Version 2.0")
-//                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-//                            distribution.set("repo")
-//                        }
-//                    }
-//                    developers {
-//                        developer {
-//                            id.set("eidam-slices")
-//                            name.set("Eidam")
-//                            url.set("https://github.com/eidam-slices")
-//                        }
-//                    }
-//                    scm {
-//                        url.set("https://github.com/eidam-slices/material-pickers")
-//                        connection.set("scm:git@github.com:eidam-slices/material-pickers.git")
-//                        developerConnection.set("scm:git@github.com:eidam-slices/material-pickers.git")
-//                    }
-//
-//                }
-//            }
-//        }
-//        signing {
-//            useGpgCmd()
-//            sign(publishing.publications["release"])
-//        }
-//    }
-//}
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+
+    coordinates(artifactId = "material-pickers")
+
+    pom {
+        name.set("Material Pickers")
+        description.set("Material 3 friendly item pickers for Compose Multiplatform (Android, iOS, Desktop)")
+        url.set("https://github.com/eidam-slices/material-pickers")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("eidam-slices")
+                name.set("Eidam")
+                url.set("https://github.com/eidam-slices")
+                email.set("eidam.slices@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/eidam-slices/material-pickers")
+            connection.set("scm:git:git://github.com/eidam-slices/material-pickers.git")
+            developerConnection.set("scm:git:ssh://git@github.com:eidam-slices/material-pickers.git")
+
+        }
+    }
+}
 
 kotlin {
 
